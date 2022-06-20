0a16f90ceaa6d9e1e51425a732fdb8f41682a509
Savannah <savfreire99@gmail.com>
2022-06-20 17:58:31 +0100

    mailgun

--- config/settings/production.py
+++ config/settings/production.py
@@ -124,9 +124,6 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://anymail.readthedocs.io/en/stable/esps/mailgun/
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 ANYMAIL = {
-    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
-    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
 }
 
 # Collectfast
