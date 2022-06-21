ef04c616fae365a6e16a44cacc6f7d94b16b5be7
Savannah <savfreire99@gmail.com>
2022-06-21 16:06:38 +0100

    blog

--- my_blog/utils/storages.py
+++ my_blog/utils/storages.py
@@ -2,11 +2,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = "static"
+    location = "blog/static"
     default_acl = "public-read"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = "media"
+    location = "blog/media"
     file_overwrite = False
     default_acl = "public-read"
